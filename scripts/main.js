74d7ab866ad393434748aca0e1bee68b395744eb
Aron Tsang <arontsang@gmail.com>
2015-12-25 16:31:02 +0800

    Support large resolutions

--- scripts/main.js
+++ scripts/main.js
@@ -60,13 +60,16 @@
                         dts : t,
                         pts :  t
                     };
-                    h264.push({
-                        data : new Uint8Array([0x00, 0x00, 0x00, 0x01, 0x09]),
-                        trackId : 0,
-                        type : 'video',
-                        dts : t,
-                        pts :  t
-                    });
+                    if(data[0] == 0 && data[1] == 0 && data[2] == 0 && data[3] == 1)
+                    {
+                        h264.push({
+                            data : new Uint8Array([0x00, 0x00, 0x00, 0x01, 0x09]),
+                            trackId : 0,
+                            type : 'video',
+                            dts : t,
+                            pts :  t
+                        });
+                    }
                     h264.push(packet);
 				};
 				fileReader.readAsArrayBuffer(e.data);
